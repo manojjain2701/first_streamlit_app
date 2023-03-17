a3066a74c306afc5971880ce4af56c9b357a7401
manojjain2701 <128174986+manojjain2701@users.noreply.github.com>
2023-03-17 12:36:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ import streamlit
 streamlit.title('My Parents new healthy diner')
 
 streamlit.header('Breakfast Menu')
+
+streanlit.text('Oatmeal and resin Bread')
+streanlit.text('Kale Spinach and Rocket Smoothie')
+streanlit.text('Hard Boiled Free range Egg')
