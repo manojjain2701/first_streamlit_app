e4ac6922122ede17dad07b3282ceecfb01a7b1d4
manojjain2701 <128174986+manojjain2701@users.noreply.github.com>
2023-03-17 12:55:15 -0500

    Update streamlit_app.py
    Added a new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text('🥣Oatmeal and resin Bread')
 streamlit.text('🥗Kale Spinach and Rocket Smoothie')
 streamlit.text('🐔Hard Boiled Free range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
